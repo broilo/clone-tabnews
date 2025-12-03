f1220397d886c206cf36fa1364a84acce129beea
Mateus Broilo <mateus.broilo90@gmail.com>
2025-12-03 13:55:57 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Aline, eu te amo! Queres café ou um tereré geladinho?</h1>
+function Home() {
+  return <h1>Aline, eu te amo! Queres café ou um tereré geladinho?</h1>;
 }
 
 export default Home;
