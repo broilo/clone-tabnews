9b0e7c923ad149ff467e7dae42efbb3a8b20db54
Mateus Broilo <mateus.broilo90@gmail.com>
2025-12-19 16:57:04 +0000

    remove console.log from tdd

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -12,6 +12,4 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.dependencies.database.max_connections).toBe(100);
   expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(0);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
-
-  console.log(responseBody);
 });
